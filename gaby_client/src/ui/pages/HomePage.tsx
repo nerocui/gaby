26ea8aa0db925a84a3d8c9b5e4a3fe7d9029d760
nerocui <nerocui@outlook.com>
2020-02-26 20:43:12 -0800

    Added app bar

--- gaby_client/src/ui/pages/HomePage.tsx
+++ gaby_client/src/ui/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import AppBarHome from '../components/AppBarHome';
 
 const HomePage = (props: any) => {
     return (
         <div>
+            <AppBarHome />
             Home Page
         </div>
     );
