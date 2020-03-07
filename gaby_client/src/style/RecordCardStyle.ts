3cc084d4c6e6b24f99cc10f2ccb4aea4ef75dfcb
nerocui <nerocui@outlook.com>
2020-03-06 17:08:42 -0800

    changed style to neumorphic

--- gaby_client/src/style/RecordCardStyle.ts
+++ gaby_client/src/style/RecordCardStyle.ts
@@ -1,6 +1,14 @@
 import {createUseStyles} from 'react-jss';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = createUseStyles({
+export const useMaterialStyles = makeStyles({
+	buttonIcon: {
+		height: '100%',
+		width: '100%',
+	}
+});
+
+export const useStyles = createUseStyles({
 	root: {
 		position: 'relative',
 		width: '100%',
@@ -12,13 +20,47 @@ const useStyles = createUseStyles({
 		width: '100%',
 		borderRadius: '1rem',
 		padding: '.5rem',
-		height: '5rem',
-		border: '1px solid black',
-		marginBottom: '1rem',
+		height: 'auto',
+		//borderBottom: '1px solid black',
+		// marginBottom: '1rem',
+		// '&:hover': {
+		// 	boxShadow:  '6px 6px 12px #c2c2c2,-6px -6px 12px #e0e0e0',
+		// },
+		boxShadow:  '6px 6px 12px #c2c2c2,-6px -6px 12px #e0e0e0',
+		transition: 'all 400ms',
+		marginBottom: '1rem'
+	},
+	flexContainer: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'flex-end',
+	},
+	personContainer: {
+		padding: '.2rem .5rem',
+		borderRadius: '1rem',
+		background: '#d1d1d1',
+		marginRight: '.5rem',
+		color: '#636363',
+		boxShadow:  '4px 4px 8px #bababa,-4px -4px 8px #e8e8e8',
 	},
 	gap: {
-		marginTop: '1rem'
+		marginTop: '.7rem',
+	},
+	horizontalGap: {
+		marginRight: '.7rem',
+	},
+	button: {
+		height: '2.5rem',
+		width: '2.5rem',
+		borderRadius: '50%',
+		boxShadow:  '5px 5px 10px #9b9b9b,-5px -5px 10px #ffffff',
+		display: 'relative',
+		background: 'linear-gradient(145deg, #bcbcbc, #e0e0e0)',
+		marginRight: '.5rem',
+		transition: 'all 200ms',
+		cursor: 'pointer',
+		'&:active': {
+			boxShadow:  '5px 5px 10px #bcbcbc,-5px -5px 10px #e6e6e6'
+		}
 	},
 });
-
-export default useStyles;
--- gaby_client/src/ui/components/RecordList.tsx
+++ gaby_client/src/ui/components/RecordList.tsx
@@ -1,21 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Record, State, Person, Role } from '../../models';
-import useStyle from '../../style/RecordCardStyle';
+import { useStyles, useMaterialStyles} from '../../style/RecordCardStyle';
 import { RecordListProps } from '../../models/props';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 const RecordList = (props: RecordListProps) => {
-    const classes = useStyle();
+    const classes = useStyles();
+    const materialClasses = useMaterialStyles();
     return (
         <div className={classes.root}>
             {props.items.map((item: Record) => {
-                const address: string = item.streetAddress + ' ' + item.city + ', ' + item.postalCode;
+                const address: string = item.streetAddress + ', ' + item.city + ', ' + item.postalCode;
                 const child: Person | undefined = item.people.find((p: Person) => p.roleId === props.roles.find((r: Role) => r.name.trim().toLowerCase() === 'child')?.id);
                 return (
                     <div key={item.id} className={classes.recordContainer}>
-                        {child && child.displayName}
+                        <div className={classes.flexContainer}>
+                            <div>
+                                <div className={classes.flexContainer}>
+                                    <h3 className={classes.horizontalGap}>{child && child.displayName}</h3>
+                                    <p>{item.fileNumber}</p>
+                                </div>
+                            </div>
+
+                            <h4>
+                                {address}
+                            </h4>
+                        </div>
                         <div>
-                            {address}
+                            <p>{item.cancerType}</p>
+                        </div>
+                        <div className={classes.gap}></div>
+                        <div  className={classes.flexContainer}>
+                            <div>
+                                <div className={classes.flexContainer}>
+                                    {item.people.map((p: Person) => {
+                                        return (
+                                            <div className={classes.personContainer}>
+                                                {p.displayName}
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
+                            <div className={classes.button}>
+                                <KeyboardArrowRightIcon className={materialClasses.buttonIcon}/>
+                            </div>
                         </div>
                     </div>
                 );
